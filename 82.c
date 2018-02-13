c408777fb23ccc74dd7680011f15e8e8798ab528
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-02-13 16:12:47 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float a,b,c;
+    printf("enter the length and breadth of the farm:");
+    scanf("%f%f",&a,&b);
+    c=a*b;
+    printf("the area of the farm is %f",c);
+ 
+}
