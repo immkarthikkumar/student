b6e751b613163f5404d26ab72b081d505922f44b
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-02-13 16:16:14 +0530

    Update 81.c

--- 81.c
+++ 81.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 void main()
 {
-	int k[20],o[20],i,n;
+	int k[20],o[20],i,a;
 	printf("Enter the Number of Groups:");
-	scanf("%d",&n);
+	scanf("%d",&a);
 	printf("\nNumber of Ninja's in Kabali and Opponent's group:");
-	for(i=0;i<n;i++)
+	for(i=0;i<a;i++)
 	{
 		scanf("%d%d",&k[i],&o[i]);
 	}
-	for(i=0;i<n;i++)
+	for(i=0;i<a;i++)
 	{
 		printf("\n%d",o[i]-k[i]);
 	}
