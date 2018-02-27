65dedac6213c735407bc9aa7d3c36b5c2633a8bf
immkarthikkumar <30829202+immkarthikkumar@users.noreply.github.com>
2018-02-27 16:43:56 +0530

    Update 97.c

--- 97.c
+++ 97.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+void main()
+{
+    int a,b,n;
+    printf("enter the number:");
+    scanf("%d",&a);
+    while(a!=0)
+    {
+        b=a%10;
+        n=n*10+b;
+        a/=10;
+    }
+    printf("%d",n);
+}
+    
